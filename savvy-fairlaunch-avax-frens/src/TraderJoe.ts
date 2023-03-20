8f4ec39983439cf2e725a58f68c79319b0d0132f
Akshay Gupta <akshaygupta54321@gmail.com>
2023-03-20 12:36:13 -0400

    all tj pools

--- savvy-fairlaunch-avax-frens/src/TraderJoe.ts
+++ savvy-fairlaunch-avax-frens/src/TraderJoe.ts
@@ -2,6 +2,14 @@ import { Transfer as TransferEventJOE } from "../generated/JOE/JOEToken";
 import { Transfer as TransferEventTJAVAXUSDC } from "../generated/TJAVAXUSDC/TJAVAXUSDCToken";
 import { Transfer as TransferEventTJBTCbAVAX } from "../generated/TJBTCbAVAX/TJBTCbAVAXToken";
 import { Transfer as TransferEventTJJOEAVAX } from "../generated/TJJOEAVAX/TJJOEAVAXToken";
+import { Transfer as TransferEventTJWBTCeBTCb } from "../generated/TJWBTCeBTCb/TJWBTCeBTCbToken";
+import { Transfer as TransferEventTJAVAXUSDT } from "../generated/TJAVAXUSDT/TJAVAXUSDTToken";
+import { Transfer as TransferEventTJUSDTUSDC } from "../generated/TJUSDTUSDC/TJUSDTUSDCToken";
+import { Transfer as TransferEventTJUSDCeUSDC } from "../generated/TJUSDCeUSDC/TJUSDCeUSDCToken";
+import { Transfer as TransferEventTJUSDTeUSDT } from "../generated/TJUSDTeUSDT/TJUSDTeUSDTToken";
+import { Transfer as TransferEventTJBTCbUSDC } from "../generated/TJBTCbUSDC/TJBTCbUSDCToken";
+import { Transfer as TransferEventTJDAIeUSDC } from "../generated/TJDAIeUSDC/TJDAIeUSDCToken";
+import { Transfer as TransferEventTJsAVAXAVAX } from "../generated/TJsAVAXAVAX/TJsAVAXAVAXToken";
 import { getUser } from "./utils";
 
 export function handleTransferJOE(event: TransferEventJOE): void {
@@ -28,6 +36,62 @@ export function handleTransferTJBTCbAVAX(event: TransferEventTJBTCbAVAX): void {
 export function handleTransferTJJOEAVAX(event: TransferEventTJJOEAVAX): void {
   const user = getUser(event.params.to.toHexString());
   user.everHeldTJJOEAVAX = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJWBTCeBTCb(event: TransferEventTJWBTCeBTCb): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJWBTCeBTCb = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJAVAXUSDT(event: TransferEventTJAVAXUSDT): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJAVAXUSDT = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJUSDTUSDC(event: TransferEventTJUSDTUSDC): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJUSDTUSDC = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJUSDCeUSDC(event: TransferEventTJUSDCeUSDC): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJUSDCeUSDC = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJUSDTeUSDT(event: TransferEventTJUSDTeUSDT): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJUSDTeUSDT = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJBTCbUSDC(event: TransferEventTJBTCbUSDC): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJBTCbUSDC = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJDAIeUSDC(event: TransferEventTJDAIeUSDC): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJDAIeUSDC = true;
+  user.everHeldAnyTraderJoe = true;
+  user.save();
+}
+
+export function handleTransferTJsAVAXAVAX(event: TransferEventTJsAVAXAVAX): void {
+  const user = getUser(event.params.to.toHexString());
+  user.everHeldTJsAVAXAVAX = true;
   user.everHeldAnyTraderJoe = true;
   user.save();
 }

fad913042554ecf9da23578d5a70acb01f11f572
Ramsey Khadder <ramseykhadder@hotmail.com>
2023-07-17 22:09:58 -0700

    Save protocol numbers

--- savvy-traderjoe-2/src/helpers/liquidity-pool.ts
+++ savvy-traderjoe-2/src/helpers/liquidity-pool.ts
@@ -6,6 +6,7 @@ import { normalizeToEighteenDecimals } from '../utils/tokens';
 import { getLPPairInUSD } from '../utils/trader-joe';
 import { getOrCreateToken } from './token';
 import { createLiquidityPoolSnapshot } from './liquidity-pool-snapshot';
+import { updateProtocolPoolLiquidity } from './protocol';
 
 export function getOrCreateLiquidityPool(contractAddress: Address): LiquidityPool {
   const id = contractAddress.toHexString();
@@ -48,5 +49,6 @@ export function updateLiquidityPool(contractAddress: Address, block: ethereum.Bl
   liquidityPool.lastUpdatedTimestamp = block.timestamp;
   liquidityPool.save();
   createLiquidityPoolSnapshot(contractAddress, block, liquidityPool);
+  updateProtocolPoolLiquidity(liquidityPool);
   return liquidityPool;
 }
--- savvy-traderjoe-2/src/helpers/protocol.ts
+++ savvy-traderjoe-2/src/helpers/protocol.ts
@@ -1,5 +1,5 @@
-import { Protocol } from "../../generated/schema";
-import { BIGDECIMAL_ZERO, BIGINT_ZERO, PROTOCOL_NAME, PROTOCOL_SLUG } from "../constants";
+import { LiquidityPool, Protocol } from "../../generated/schema";
+import { BIGDECIMAL_ZERO, BIGINT_ZERO, PROTOCOL_NAME, PROTOCOL_SLUG, TJ_LP_SVBTC, TJ_LP_SVETH, TJ_LP_SVUSD } from "../constants";
 
 export function getOrCreateProtocol(): Protocol {
   let protocol = Protocol.load(PROTOCOL_SLUG);
@@ -7,11 +7,48 @@ export function getOrCreateProtocol(): Protocol {
     protocol = new Protocol(PROTOCOL_SLUG);
     protocol.name = PROTOCOL_NAME;
     protocol.numOfAccounts = BIGINT_ZERO;
+    protocol.svUSD = BIGINT_ZERO;
+    protocol.USDC = BIGINT_ZERO;
     protocol.svUSDLiquidityUSD = BIGDECIMAL_ZERO;
+    protocol.svETH = BIGINT_ZERO;
+    protocol.WETH = BIGINT_ZERO;
     protocol.svETHLiquidityUSD = BIGDECIMAL_ZERO;
+    protocol.svBTC = BIGINT_ZERO;
+    protocol.WBTC = BIGINT_ZERO;
     protocol.svBTCLiquidityUSD = BIGDECIMAL_ZERO;
+    protocol.totalLiquidityUSD = BIGDECIMAL_ZERO;
+    protocol.lastUpdatedBN = BIGINT_ZERO;
+    protocol.lastUpdatedTimestamp = BIGINT_ZERO;
     protocol.save();
   }
 
+  return protocol;
+}
+
+export function updateProtocolPoolLiquidity(lbPair: LiquidityPool): Protocol {
+  const protocol = getOrCreateProtocol();
+
+  if (lbPair.id.includes(TJ_LP_SVUSD)) {
+    protocol.svUSD = lbPair.savvySyntheticBalance;
+    protocol.USDC = lbPair.pairTokenBalanceNormalized;
+    protocol.svUSDLiquidityUSD = lbPair.totalValueUSD;
+  }
+
+  if (lbPair.id.includes(TJ_LP_SVETH)) {
+    protocol.svETH = lbPair.savvySyntheticBalance;
+    protocol.WETH = lbPair.pairTokenBalanceNormalized;
+    protocol.svETHLiquidityUSD = lbPair.totalValueUSD;
+  }
+
+  if (lbPair.id.includes(TJ_LP_SVBTC)) {
+    protocol.svBTC = lbPair.savvySyntheticBalance;
+    protocol.WBTC = lbPair.pairTokenBalanceNormalized;
+    protocol.svBTCLiquidityUSD = lbPair.totalValueUSD;
+  }
+
+  protocol.totalLiquidityUSD = protocol.svBTCLiquidityUSD.plus(protocol.svETHLiquidityUSD).plus(protocol.svUSDLiquidityUSD);
+  protocol.lastUpdatedBN = lbPair.lastUpdatedBN;
+  protocol.lastUpdatedTimestamp = lbPair.lastUpdatedTimestamp;
+  protocol.save();
   return protocol;
 }

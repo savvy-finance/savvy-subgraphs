ce789901d935192bd8f59a7a774d0d8622e473ca
Web | Mobile | Blockchain Full-Stack Engineer <29835081+mmesser-rr@users.noreply.github.com>
2023-08-22 20:42:27 +0300

    Track all veSVY holders (#21)

--- svy/src/helpers/balance.ts
+++ svy/src/helpers/balance.ts
@@ -1,5 +1,7 @@
 import { Address, BigInt, ethereum } from "@graphprotocol/graph-ts";
 import { Account } from "../../generated/schema";
+import { veSVY } from "../../generated/veSVY/veSVY";
+import { VE_SVY } from "../constants";
 import { getSvyBalanceInUSD } from "../utils/tokens";
 import { getOrCreateAccount } from "./account";
 
@@ -17,6 +19,15 @@ export function sendSVY(accountAddress: Address, svySent: BigInt, block: ethereu
   const account = getOrCreateAccount(accountAddress);
   account.svyBalance = account.svyBalance.minus(svySent);
   account.svyBalanceUSD = getSvyBalanceInUSD(account.svyBalance);
+  account.lastUpdatedBN = block.number;
+  account.lastUpdatedTimestamp = block.timestamp;
+  account.save();
+  return account;
+}
+
+export function updateVeSVYBalance(accountAddress: Address, block: ethereum.Block): Account {
+  const account = getOrCreateAccount(accountAddress);
+  account.veSVYBalance = veSVY.bind(Address.fromString(VE_SVY)).balanceOf(accountAddress);
   account.lastUpdatedBN = block.number;
   account.lastUpdatedTimestamp = block.timestamp;
   account.save();

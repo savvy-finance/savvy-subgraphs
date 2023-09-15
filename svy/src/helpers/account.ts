438d18fd5c0df3334f332db73b111f0bc41aff7c
Web | Mobile | Blockchain Full-Stack Engineer <29835081+mmesser-rr@users.noreply.github.com>
2023-09-14 20:04:13 -0700

    Add svy, veSVY earn rate per second and max veSVY earnable (#28)

--- svy/src/helpers/account.ts
+++ svy/src/helpers/account.ts
@@ -3,6 +3,7 @@ import { Account } from "../../generated/schema";
 import {
   BIGDECIMAL_ZERO,
   BIGINT_ZERO,
+  SFIAContract,
   ZERO_ADDRESS,
   veSVYContract
 } from "../constants";
@@ -29,6 +30,9 @@ export function getOrCreateAccount(address: Address): Account {
     account.svyBalanceUSD = BIGDECIMAL_ZERO;
     account.veSVYBalance = BIGINT_ZERO;
     account.stakedSVY = BIGINT_ZERO;
+    account.svyEarnRatePerSec = BIGINT_ZERO;
+    account.veSVYEarnRatePerSec = BIGINT_ZERO;
+    account.maxVeSvyEarnable = BIGINT_ZERO;
     account.lastUpdatedBN = BIGINT_ZERO;
     account.lastUpdatedTimestamp = BIGINT_ZERO;
     account.save();
@@ -46,6 +50,11 @@ export function receiveSVY(accountAddress: Address, svyReceived: BigInt, block: 
   const oldSVYBalance = account.svyBalance;
   account.svyBalance = account.svyBalance.plus(svyReceived);
   account.svyBalanceUSD = getSVYBalanceInUSD(account.svyBalance);
+
+  const mySVYPageInfo = SFIAContract.getMySVYPageInfo(accountAddress);
+  account.svyEarnRatePerSec = mySVYPageInfo.svyEarnRatePerSec;
+  account.veSVYEarnRatePerSec = mySVYPageInfo.veSVYEarnRatePerSec;
+  account.maxVeSvyEarnable = mySVYPageInfo.maxVeSvyEarnable;
   account.lastUpdatedBN = block.number;
   account.lastUpdatedTimestamp = block.timestamp;
   account.save();
@@ -69,6 +78,10 @@ export function sendSVY(accountAddress: Address, svySent: BigInt, block: ethereu
   const oldSVYBalance = account.svyBalance;
   account.svyBalance = account.svyBalance.minus(svySent);
   account.svyBalanceUSD = getSVYBalanceInUSD(account.svyBalance);
+  const mySVYPageInfo = SFIAContract.getMySVYPageInfo(accountAddress);
+  account.svyEarnRatePerSec = mySVYPageInfo.svyEarnRatePerSec;
+  account.veSVYEarnRatePerSec = mySVYPageInfo.veSVYEarnRatePerSec;
+  account.maxVeSvyEarnable = mySVYPageInfo.maxVeSvyEarnable;
   account.lastUpdatedBN = block.number;
   account.lastUpdatedTimestamp = block.timestamp;
   account.save();

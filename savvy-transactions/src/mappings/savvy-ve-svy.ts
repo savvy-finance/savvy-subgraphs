758015951e31c591fa1f026be6399d8c213d57b3
daiki saito <daiki.saito92@gmail.com>
2023-12-04 20:57:51 -0800

    feat: track the staked svy token amount

--- savvy-transactions/src/mappings/savvy-ve-svy.ts
+++ savvy-transactions/src/mappings/savvy-ve-svy.ts
@@ -8,13 +8,20 @@ import {
   createUnstakedEvent,
   createClaimedEvent,
 } from "../helpers/savvyVeSvy";
+import {getOrCreateAccount} from "../helpers/account";
 
 export function handleStaked(event: Staked): void {
   createStakedEvent(event);
+  let account = getOrCreateAccount(event.params.user.toHexString());
+  account.stakedSVY = account.stakedSVY.plus(event.params.amount);
+  account.save();
 }
 
 export function handleUnstaked(event: Unstaked): void {
   createUnstakedEvent(event);
+  let account = getOrCreateAccount(event.params.user.toHexString());
+  account.stakedSVY = account.stakedSVY.plus(event.params.amount);
+  account.save();
 }
 
 export function handleClaimed(event: Claimed): void {

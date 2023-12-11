99c8d1921593e2ef9d8b27230087d237fc529eee
Professional Web3 Full-Stack Engineer <29835081+mmesser-rr@users.noreply.github.com>
2023-12-11 23:23:35 +0800

    SVY - Provide circulating SVY amount (#36)

--- svy/src/handlers/svy.ts
+++ svy/src/handlers/svy.ts
@@ -1,10 +1,16 @@
+import { ethereum } from "@graphprotocol/graph-ts";
 import {
   Transfer as TransferEvent
 } from "../../generated/SavvyProtocolToken/SavvyProtocolToken";
 import { receiveSVY, sendSVY } from "../helpers/account";
+import { updateCirculatingSVY } from "../helpers/protocol";
 
 export function handleTransfer(event: TransferEvent): void {
   const svyAmount = event.params.value;
   sendSVY(event.params.from, svyAmount, event.block);
   receiveSVY(event.params.to, svyAmount, event.block);
+}
+
+export function handleBlock(block: ethereum.Block): void {
+  updateCirculatingSVY(block);
 }

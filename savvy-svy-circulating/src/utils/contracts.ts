5537f017904768edd1bb18c2e6af94827ba84cfe
daiki saito <daiki.saito92@gmail.com>
2023-12-11 10:43:09 -0800

    feat: reflect comments

--- savvy-svy-circulating/src/utils/contracts.ts
+++
@@ -1,7 +0,0 @@
-import { Address } from "@graphprotocol/graph-ts";
-import { CONTRACT_TO_NAME_MAP } from "../constants";
-
-export function getFriendlyName(address: Address): string | null {
-  const hexString = address.toHexString();
-  return CONTRACT_TO_NAME_MAP.get(hexString);
-}
